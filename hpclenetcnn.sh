2dcaecc4b6404ffccd88f7caf8005990f2f850d1
Ashim Dahal <codeashim@gmail.com>
2024-07-12 14:39:52 -0500

    feat: multigpu setup and sbatch submission

---
+++ hpclenetcnn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=lenetcnn
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:p100:2
+#SBATCH --time=24:00:00
+#SBATCH --output=outputcnn.log
+#SBATCH --error=errorcnn.log
+
+# Load necessary modules or activate virtual environment
+module load cuda-toolkit/11.6.2
+module load python/3.8.6
+
+torchrun lenetcnn.py -p model_snapshots/lenetcnn -e 10 -s 2
